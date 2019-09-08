c4044524aa8cc642d246f5793cb9d2396577c261
borrik <borrik@live.ru>
2019-09-08 13:58:41 +0300

    added volunteer subscription implementation

--- app_server/controllers/volunteers.js
+++ app_server/controllers/volunteers.js
@@ -46,8 +46,37 @@ module.exports.volunteerAssignPage = function (req, res){
     })};
 
 // getting user's choise of volunteer and location - assign volunteer to location
-module.exports.volunteerAssignCommit = function (req,res) {
-    console.log('>>>>>',req.body)
+module.exports.volunteerAssignCommit = function (req,resp) {
+    console.log('>>>>>',req.body);
+    let attachvol= req.body.volunteer;
+    let locationid= req.body.location;
+
+    request(url.resolve(ApiOptions.server, "api/locations/" + locationid), {
+        method: 'get',
+        json: {}
+    }, function (err, locApiResp, locBody) {
+        if (err) {
+            console.log(err)
+        }
+        let locVolsUpdated = locBody.volunteers;
+        //creating array of id's
+        locVolsUpdated=locVolsUpdated.map(function (vol) {
+            return vol._id
+        });
+        locVolsUpdated.push(attachvol);
+
+        //third - attach this id to location object
+        request(url.resolve(ApiOptions.server, "api/locations/"+ locationid), {
+                method: 'put',
+                json: {volunteers: locVolsUpdated.toString()}},
+            function(err, updatedApiResp, updLocBody) {
+                if (err) {
+                    console.log('error',err)
+                }
+                resp.redirect('/locations/'+locationid)
+            })
+    })
+
 };
 
 
