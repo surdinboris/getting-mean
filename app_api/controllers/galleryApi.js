e7758acbb040134e17084d977df907696e042cbe
borrik <borrik@live.ru>
2019-10-28 16:16:39 +0200

    picture storing in db implemented - another implementation via schema methods

--- app_api/controllers/galleryApi.js
+++ app_api/controllers/galleryApi.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 let url = require('url');
 // let Loc = mongoose.model('locations');
 // let Cat = mongoose.model('cats');
-//let Catphoto = mongoose.model('catphotos', catPhotoSchema);
+let Catphoto = mongoose.model('catphotos');
 
 let apilib = require('../../apilib');
 let sendJsonResponse = function(res, status, content) {
@@ -104,27 +104,28 @@ module.exports.uploadPhotoToDB= function (req,res) {
             let mimetype = req.files[file].mimetype;
             let buffdata= req.files[file].data;
             let filename = req.files[file].name;
-             //Catphoto.new()
-            cat.catPhoto.push({imageData:buffdata, comment:filename,contentType:mimetype});
-            cat.save().then(function (cat) {
-                //do stuff
-                //db storing here
-                //responding
-                let catPhotos = JSON.parse(JSON.stringify(cat.catPhoto));
-                catPhotos.forEach(function(catPhoto){
-                    //catPhoto.imageData.data=Buffer.from(catPhoto.imageData.data).toString('base64');
-                    catPhoto.imageData.data=Buffer.from(catPhoto.imageData.data).toString('base64');
-                });
-                //imgdata:Buffer.from(tumb.imageData.data).toString('base64')
-                //need to filter (select) fields data only
-                //sendJsonResponse(res, 220, cats)
-                //  res.writeHead(200,{'Content-type':'image/jpg'});
-                // res.end(content);
-                //console.log(cat);
-                //   res.end(cat.catPhoto[0].imageData);
-                sendJsonResponse(res,220,catPhotos)
+            let cf=Catphoto.create({imageData:buffdata, comment:filename,contentType:mimetype}).then(cf=> {   cat.catPhoto.push(cf);
+                cat.save().then(function (cat) {
+                    //do stuff
+                    //db storing here
+                    //responding
+                    let catPhotos = JSON.parse(JSON.stringify(cat.catPhoto));
+                    catPhotos.forEach(function(catPhoto){
+                        //catPhoto.imageData.data=Buffer.from(catPhoto.imageData.data).toString('base64');
+                        catPhoto.imageData.data=Buffer.from(catPhoto.imageData.data).toString('base64');
+                    });
+                    //imgdata:Buffer.from(tumb.imageData.data).toString('base64')
+                    //need to filter (select) fields data only
+                    //sendJsonResponse(res, 220, cats)
+                    //  res.writeHead(200,{'Content-type':'image/jpg'});
+                    // res.end(content);
+                    //console.log(cat);
+                    //   res.end(cat.catPhoto[0].imageData);
+                    sendJsonResponse(res,220,catPhotos)
 
-            })
+                })})
+           // cat.catPhoto.push({imageData:buffdata, comment:filename,contentType:mimetype});
+
         }
 
         //console.log("+++",req.files);
